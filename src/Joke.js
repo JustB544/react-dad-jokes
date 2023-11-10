547bf02cb8b60ec216e6bb863e170133c7595cd5
Breyton Pabst <breytonpabst@gmail.com>
2023-11-10 14:24:03 -0700

    reworked to use hooks

--- src/Joke.js
+++ src/Joke.js
@@ -3,10 +3,7 @@ import "./Joke.css";
 
 /** A single joke, along with vote up/down buttons. */
 
-class Joke extends Component {
-  render() {
-    const { id, vote, votes, text } = this.props;
-
+function Joke({id, vote, votes, text}) {
     return (
       <div className="Joke">
         <div className="Joke-votearea">
@@ -24,7 +21,6 @@ class Joke extends Component {
         <div className="Joke-text">{text}</div>
       </div>
     );
-  }
 }
 
 export default Joke;
